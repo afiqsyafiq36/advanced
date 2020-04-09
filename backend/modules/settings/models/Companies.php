3665a4f81bbc8d84e496fd485d5e632492496bff
afiqsyafiq36 <afiqsyaf36@gmail.com>
2020-04-10 00:07:46 +0800

    Yii2 Lesson - 13 Bootstrap Style Datepicker

--- backend/modules/settings/models/Companies.php
+++ backend/modules/settings/models/Companies.php
@@ -11,6 +11,7 @@ use Yii;
  * @property string $company_name
  * @property string $company_email
  * @property string $company_address
+ * @property string|null $company_start_date
  * @property string|null $company_created_date
  * @property string $company_status
  *
@@ -34,7 +35,7 @@ class Companies extends \yii\db\ActiveRecord
     {
         return [
             [['company_name', 'company_email', 'company_address', 'company_status'], 'required'],
-            [['company_created_date'], 'safe'],
+            [['company_start_date', 'company_created_date'], 'safe'],
             [['company_status'], 'string'],
             [['company_name', 'company_email'], 'string', 'max' => 100],
             [['company_address'], 'string', 'max' => 255],
@@ -51,6 +52,7 @@ class Companies extends \yii\db\ActiveRecord
             'company_name' => 'Company Name',
             'company_email' => 'Company Email',
             'company_address' => 'Company Address',
+            'company_start_date' => 'Company Start Date',
             'company_created_date' => 'Company Created Date',
             'company_status' => 'Company Status',
         ];
--- backend/modules/settings/views/companies/_form.php
+++ backend/modules/settings/views/companies/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\datepicker\DatePicker;
 /* @var $this yii\web\View */
 /* @var $model backend\modules\settings\models\Companies */
 /* @var $form yii\widgets\ActiveForm */
@@ -18,9 +18,27 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
 
+
+    <?php
+    // with an ActiveForm instance 
+    ?>
+    <?= $form->field($model, 'company_start_date')->widget(
+        DatePicker::className(),
+        [
+            // inline too, not bad
+            'inline' => false,
+            // modify template for custom rendering
+            // 'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-m-d',
+            ]
+        ]
+    ); ?>
+
     <?= $form->field($model, 'company_created_date')->textInput() ?>
 
-    <?= $form->field($model, 'company_status')->dropDownList([ 'active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'company_status')->dropDownList(['active' => 'Active', 'inactive' => 'Inactive',], ['prompt' => '']) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
